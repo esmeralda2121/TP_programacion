8f6d06388a4468f31b9ac586961469b972bf96a5
esmeralda2121 <145503028+esmeralda2121@users.noreply.github.com>
2023-11-18 14:37:34 -0300

    primer commit 3

---
+++ examenP/home.php
@@ -0,0 +1,31 @@
+<?php
+    session_start ();
+
+if (!isset($_SESSION['usuario'])){
+    echo '
+        <script> 
+        alert ("Por favor debes iniciar sesion");
+        window.location = "index.php";
+        </script>
+        
+    ';
+    session_destroy ();
+    die();
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+</head>
+<body>
+    <h1>
+        BIENVENIDO A LA PAGINA!
+    </h1>
+    <a href="php/cerrar_sesion.php">Cerrar sesion</a>
+</body>
+</html>
